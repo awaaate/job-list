0067bc2911b36073829710dc8b28e9d1b011f79a
awaaate <tawatetb04@gmail.com>
2020-06-27 12:32:44 +0200

    Updates

---
+++ precache-manifest.0222b68b9316e61300ef53b13aed3d87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9641751ca8c19436c9d2d6184b80326",
+    "url": "/job-list/index.html"
+  },
+  {
+    "revision": "ff0e8278b6ca359e25b6",
+    "url": "/job-list/static/css/main.3aeac22f.chunk.css"
+  },
+  {
+    "revision": "423e51082499d9703602",
+    "url": "/job-list/static/js/2.4ddb8748.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/job-list/static/js/2.4ddb8748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0e8278b6ca359e25b6",
+    "url": "/job-list/static/js/main.fec3d2f1.chunk.js"
+  },
+  {
+    "revision": "6db41e5df3d3e4b19527",
+    "url": "/job-list/static/js/runtime-main.1c60a3ad.js"
+  }
+]);
